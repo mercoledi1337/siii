0b127fcabff06eec3b664cb11e16ee7255fdabe2
mercoledi1337 <mrsereda@wp.pl>
2023-02-06 18:06:18 +0100

    entity problemy

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,20 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'movies')]
     public function index(): Response
     {
-        $movies = ["AVG: END", "inecptioon", "lok", "kdsajdsa"];
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findAll();
 
-        return $this->render('index.html.twig', array(
-            'movies' => $movies
-        ));
+        dd($movies);
+
+        return $this->render('index.html.twig');
     } 
 }
